ee9be3ef990fc2b543cc2fb66ea1663c8a8ac13a
Christian Dean Ward <christiandeanward@gmail.com>
2018-03-16 13:50:12 -0700

    Config files

---
+++ config/connection.js
@@ -0,0 +1,27 @@
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "Zulu!956",
+  database: "dj_bookingDB"
+});
+
+
+connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
+  
+  module.exports = connection;
+});
+
+}
